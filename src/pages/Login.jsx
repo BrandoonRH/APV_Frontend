b4e5a5d5f4adbb9070029936f3da46bda08ee5e1
BrandoonRH <aeroatlas.51@gmail.com>
2022-08-01 16:57:34 -0500

    Pequeño Cambio para Review

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -40,7 +40,7 @@ export const Login = () => {
            <div>
                <h1 className="text-indigo-600 font-black text-6xl">
                   Inicia Sesión y Administra tus 
-                  <span className="text-black">Pacientes</span>
+                  <span className="text-black">Pacientes.</span>
                 </h1>
            </div>
            <div className="mt-20 md:mt-5 shadow-2xl px-5 py-10 rounded-xl bg-white">
